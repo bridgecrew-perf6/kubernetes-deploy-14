e835310fabfe82658e8725b873c05d227d1c17a6
khadija <khadijajadmy99@gmail.com>
2022-01-13 12:01:23 +0100

    make ci-deploy executable

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -9,5 +9,6 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # and overwrite the original with that one. 
 envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out 
 mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml 
+
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt 
 ./kubectl --kubeconfig=/dev/null --server=$KUBERNETES_SERVER --certificate-authority=cert.crt --token=$KUBERNETES_TOKEN apply -f ./kube/
